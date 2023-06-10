7e5f6296ed1653377c03ddf0258583d3ee00a159
pethiprojs <63162775+pethiprojs@users.noreply.github.com>
2023-06-10 00:33:21 +0000

    updating to system-model version of chatgpt

--- readNGo.py
+++ readNGo.py
@@ -7,16 +7,26 @@ from executa import executa
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_CHATGPT_API_KEY")
-standard_text = "Meu código já possui o código de 3 API's em Python.\n\n aud_in(), ao ser chamada grava 30 segundos de áudio do usuário.\n\nwhisper_input(aud),  recebe o áudio gravado e transcreve o áudio para texto.\n\nchat_input(text), recebe o texto transcrito e envia para uma IA processar o texto e retorna um código em python.\n\nmy_bedroom(code), recebe e executa o código caso o comando de texto tenha sido para o quarto.\n\nmy_kit(code), recebe e executa o código caso o comando de texto tenha sido para a cozinha.\n\nutilizando as APIs já PRONTAS, descritas anteriormente, converta o seguinte texto em código python utilizando as funções anteriores:\n\n'"
+standard_text = "Meu código já possui o código de 3 API's em Python.\n\n aud_in(), ao ser chamada grava 30 segundos de áudio do usuário.\n\nwhisper_input(aud),  recebe o áudio gravado e transcreve o áudio para texto.\n\nchat_input(text), recebe o texto transcrito e envia para uma IA processar o texto e retorna um código em python.\n\nmy_bedroom(code), recebe e executa o código caso o comando de texto tenha sido para o quarto.\n\nmy_kit(code), recebe e executa o código caso o comando de texto tenha sido para a cozinha."
 
 def gpt_input(text_in):
-    prompt_text = standard_text + text_in + "'"
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt_text,
+    prompt_text = "utilizando as APIs já PRONTAS, descritas anteriormente, converta o seguinte texto em código python utilizando as funções anteriores:\n\n'" + text_in
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = [
+            {
+                "role":"system",
+                "content": standard_text
+            },
+            {
+                "role":"user",
+                "content": prompt_text
+            }
+        ],
+        temperature = 0,
         max_tokens=300
     )
-    transcription = response.choices[0].text.strip()
+    transcription = response.choices[0].message.content.strip()
     # the json file to save the output data   
     save_file = open("savedata.json", "w")  
     json.dump(response, save_file, indent = 6)  
