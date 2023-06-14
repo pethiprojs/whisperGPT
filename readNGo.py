0efb00d4ef83cacd784b9d9c71c1c0dff6e12927
pethiprojs <63162775+pethiprojs@users.noreply.github.com>
2023-06-14 13:23:20 +0000

    version 1.1.3 - testing function call

--- readNGo.py
+++ readNGo.py
@@ -9,6 +9,8 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_CHATGPT_API_KEY")
 standard_text = "Meu código já possui o código de 3 API's em Python.\n\n aud_in(), ao ser chamada grava 30 segundos de áudio do usuário.\n\nwhisper_input(aud),  recebe o áudio gravado e transcreve o áudio para texto.\n\nchat_input(text), recebe o texto transcrito e envia para uma IA processar o texto e retorna um código em python.\n\nmy_bedroom(code), recebe e executa o código caso o comando de texto tenha sido para o quarto.\n\nmy_kit(code), recebe e executa o código caso o comando de texto tenha sido para a cozinha."
 
+
+
 def gpt_input(text_in):
     prompt_text = "utilizando as APIs já PRONTAS, descritas anteriormente, converta o seguinte texto em código python utilizando as funções anteriores:\n\n'" + text_in
     response = openai.ChatCompletion.create(
@@ -23,21 +25,44 @@ def gpt_input(text_in):
                 "content": prompt_text
             }
         ],
+        functions=[
+            {
+                "name": "executa",
+                "description": "execute a given code",
+                "parameters": {
+                    "type": "string",
+                    "properties": {
+                        "code": {
+                            "type": "string",
+                            "description": "A code to run and execute the user-demanded-action",
+                        },
+                        # "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+                    },
+                    "required": ["code"],
+                },
+            }
+        ],
+        function_call="auto",
         temperature = 0,
         max_tokens=300
     )
+    message = response.choices[0].message
     transcription = response.choices[0].message.content.strip()
     # the json file to save the output data   
     save_file = open("savedata.json", "w")  
-    json.dump(response, save_file, indent = 6)  
-    save_file.close()  
+    json.dump(response, save_file, indent = 6)
+    save_file.close()
+
+    # if message.get("function_call"):
+    #     function_name = message["function_call"]["name"]
+        
 
     return transcription
 
 
 gpt_code = gpt_input("ligue as luzes da sala")
-print(gpt_code)
+# print(gpt_code)
 # gpt_code = "print('executando com sucesso')"
-executa(gpt_code)
+# executa(gpt_code)
 
 # https://community.openai.com/t/providing-context-to-the-chat-api-before-a-conversation/195853/3
